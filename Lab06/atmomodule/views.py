8222709e651736167e3771403957f173fdf728a2
IAmIllusion <artemx8zx@gmail.com>
2019-12-06 11:29:55 +0200

    Lab06 final

--- Lab06/atmomodule/views.py
+++ Lab06/atmomodule/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Sensor
+from .models import Sensor, SensorCategory
 
 
 def index(request):
@@ -8,8 +8,9 @@ def index(request):
 
 def sensor(request, id):
     sensor = Sensor.objects.filter(id=id).values()
-    return render(request, "sensor.html", context={"sensor": sensor})
+    category = SensorCategory.objects.filter(id=list(sensor)[0]['category_id']).values()
+    return render(request, "sensor.html", context={"sensor": list(sensor)[0], "category": list(category)[0]})
 
 def sensors(request):
     sensors = Sensor.objects.all()
-    return render(request, "sensors.html", context={"sensors": sensors})
+    return render(request, "sensors.html", context={"sensors": list(sensors)})
