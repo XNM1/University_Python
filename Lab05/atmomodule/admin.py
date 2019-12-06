8cd915770d5ce6b47ef000e030fafc498a49375a
IAmIllusion <artemx8zx@gmail.com>
2019-12-06 09:31:58 +0200

    lab05 fixes

--- Lab05/atmomodule/admin.py
+++ Lab05/atmomodule/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import SensorCategory
+from .models import Sensor
+from .models import AtmoSnapshot
+
 
 # Register your models here.
+admin.site.register(SensorCategory)
+admin.site.register(Sensor)
+admin.site.register(AtmoSnapshot)
--- Lab05/atmomodule/models.py
+++ Lab05/atmomodule/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class SensorCategory(models.Model):
     name = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return self.name
+
 class Sensor(models.Model):
     lon = models.FloatField()
     lat = models.FloatField()
@@ -11,6 +14,9 @@ class Sensor(models.Model):
     model = models.CharField(max_length = 100)
     category = models.ForeignKey(SensorCategory, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class AtmoSnapshot(models.Model):
     date = models.DateField(auto_now = True)
     time = models.TimeField(auto_now = True)
@@ -18,3 +24,6 @@ class AtmoSnapshot(models.Model):
     pressure = models.FloatField()
     co2_level = models.FloatField()
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.date) + " " + str(self.time)
